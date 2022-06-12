7cf7b986da2333b090750450d10308906b45bb6c
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-12 16:24:09 -0300

    Update ProdMercadoBean.java

--- FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/jpa/ejbbean/ProdMercadoBean.java
+++ FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/jpa/ejbbean/ProdMercadoBean.java
@@ -8,16 +8,19 @@ import javax.persistence.PersistenceContext;
 
 import br.com.senai.Naara.application.model.Produtos;
 
-@Stateless
+@Stateless //Utilizado quando você tem uma classe que representa apenas um serviço, mas sem precisar guardar informações.
+
 
 public class ProdMercadoBean {
 	
-	@PersistenceContext
+	@PersistenceContext //é um local onde ficam armazenados os objetos (entidades) que estão sendo manipulados pelo EntityManager corrente
 	
-	private EntityManager em;
+	private EntityManager em; //EntityManager= Provê APIs para criar consultas, buscando objetos, sincronizando objetos, e inserindo objetos no banco de dados
 	
 	public List<ProdMercadoBean> listar() {
 		return em.createQuery("SELECT c FROM prodmercado c", ProdMercadoBean.class).getResultList();
+		
+		//obs Naara: createQuery = usado para consultar o armazenamento de dados usando consultas de linguagem de consulta Java Persistence.
 	}
 }
 
