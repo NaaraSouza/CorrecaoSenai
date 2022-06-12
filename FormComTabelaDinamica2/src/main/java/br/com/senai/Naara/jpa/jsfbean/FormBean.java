765d56fe87dc1284fa47eda7eeef55e3671dcebd
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-12 16:28:22 -0300

    Update FormBean.java

--- FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/jpa/jsfbean/FormBean.java
+++ FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/jpa/jsfbean/FormBean.java
@@ -16,23 +16,24 @@ import br.com.senai.Naara.jpa.ejbbean.ComandosBean;
 
 
 
-@Named("form")
-@RequestScoped
-public class FormBean implements Serializable {
+@Named("form") //É usado parfa fazer a Inversão de Controle e Injeção de Dependências.
+@RequestScoped // Um objeto que contém como informações visíveis // Um objeto que contém um método
+
+public class FormBean implements Serializable { // O implementa Serializable basicamente serve para habilitar que um objeto possa de uma determinada classe ter seu estado persistente pela API padrão do java
 	
-	@EJB
+	@EJB //ajuda fazer ligação com o banco de dados
 	private ComandosBean comandosBean;
 	
-	@Inject
-	private FacesContext context;
+	@Inject //nos permite definir um ponto de injeção que é injetado durante a instanciação do bean.
+	private FacesContext context; //FacesContext contém todas as informações de estado por solicitação relacionadas ao processamento de uma única solicitação JavaServer Faces e à renderização da resposta correspondente.
 	
-	private UIComponent searchInputText;
+	private UIComponent searchInputText; //UIComponent é a classe base para todos os componentes da interface do usuário no JavaServer Faces.
 	
 	private Integer tarefaId;
 	
 	private Despesas despesa1;
 	
-	public void gravarr(AjaxBehaviorEvent event) {
+	public void gravarr(AjaxBehaviorEvent event) { //AjaxBehaviorEvent representa o comportamento do componente específico para Ajax
 		if(despesa1.getId()==null) {
 			comandosBean.inserir(despesa1);
 		}	
