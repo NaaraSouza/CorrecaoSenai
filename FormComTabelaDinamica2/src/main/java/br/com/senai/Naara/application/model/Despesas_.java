5c9219ff5852746c2bfd469917f3a8fc91f72b4e
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-12 16:16:04 -0300

    Update Despesas_.java

--- FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/application/model/Despesas_.java
+++ FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/application/model/Despesas_.java
@@ -4,8 +4,9 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2022-05-31T14:54:05.699-0300")
-@StaticMetamodel(Despesas.class)
+@Generated(value="Dali", date="2022-05-31T14:54:05.699-0300") // @Generated é usado para marcar o código-fonte que foi gerado. Ele também pode ser usado para diferenciar o código escrito pelo usuário do código gerado em um único arquivo.
+@StaticMetamodel(Despesas.class) // @StaticMetamodel permite gerar e referenciar estaticamente os atributos de cada entidade do seu modelo.
+
 public class Despesas_ {
 	public static volatile SingularAttribute<Despesas, String> data;
 	public static volatile SingularAttribute<Despesas, String> descricao;
