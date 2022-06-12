7ad2a5cca5268eb260bc9a85faff4b7650cb7ce3
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-12 16:11:41 -0300

    Update Config.java

--- FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/application/config/Config.java
+++ FormComTabelaDinamica2/src/main/java/br/com/senai/Naara/application/config/Config.java
@@ -4,7 +4,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
 
-@FacesConfig(version = Version.JSF_2_3) 
-@ApplicationScoped
+@FacesConfig(version = Version.JSF_2_3) //) // Faces fornece,comportamentos e recursos que são associados ao JSF.
+@ApplicationScoped //É usado para indicar que só haverá um objeto da classe anotada com @ApplicationScoped na sua aplicação.
 public class Config {
 }
